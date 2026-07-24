0d53ed4a7d0182768d7a041ddff0b9493a5bee25
mgokul92006 <mgokul92006@gmail.com>
2026-07-24 18:11:45 +0530

    Time: 79 ms (5.08%), Space: 27.8 MB (6.18%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-    int dpCalculate(string& s1,string& s2,int i,int j,vector<vector<int>>&dp){
-        if(s1.size()<=i || s2.size()<=j)
+    int dpCalculate(string& t1,string& t2,int i,int j,vector<vector<int>>&dp){
+        if(i==t1.size() || j==t2.size())
         return 0;
         if(dp[i][j]!=-1)
         return dp[i][j];
-        int take=0,nontake=0;
-        if(s1[i]==s2[j])
-        take=1+dpCalculate(s1,s2,i+1,j+1,dp);
-        nontake=max(dpCalculate(s1,s2,i,j+1,dp),dpCalculate(s1,s2,i+1,j,dp));
-        return dp[i][j]=max(take,nontake);
+        int take=0,nottake=0;
+        if(t1[i]==t2[j])
+        take=1+dpCalculate(t1,t2,i+1,j+1,dp);
+        nottake=max(dpCalculate(t1,t2,i,j+1,dp),dpCalculate(t1,t2,i+1,j,dp));
+        return dp[i][j]=max(take,nottake);
     }
     int longestCommonSubsequence(string text1, string text2) {
-        vector<vector<int>>dp(1001,vector<int>(1001,-1));
+        vector<vector<int>>dp(text1.size(),vector<int>(text2.size(),-1));
         return dpCalculate(text1,text2,0,0,dp);
     }
 };
