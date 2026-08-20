1c26b935f6c3db027df0cbc8d686a049e490e177
mgokul92006 <mgokul92006@gmail.com>
2026-08-20 12:32:23 +0530

    Time: 1426 ms (16.31%), Space: 130.7 MB (12.67%) - LeetHub

--- 1928-minimum-cost-to-reach-destination-in-time/1928-minimum-cost-to-reach-destination-in-time.cpp
+++ 1928-minimum-cost-to-reach-destination-in-time/1928-minimum-cost-to-reach-destination-in-time.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
     int dij(vector<vector<pair<int,int>>>&adj,int maxTime,vector<int>& Fees,int n){
-        vector<int>dis(n,INT_MAX);
-        dis[0]=0;
+        vector<vector<int>>dis(n,vector<int>(maxTime+1,INT_MAX));
         priority_queue<tuple<int,int,int>,vector<tuple<int,int,int>>,greater<tuple<int,int,int>>>q;
         q.push({Fees[0],0,0});
         while(!q.empty()){
             auto [cost,time,node]=q.top();
             q.pop();
-            if(node==n-1)
-            return cost;
             for(auto i:adj[node]){
                 int newCost=cost+Fees[i.first];
                 int newTime=time+i.second;
-                if(dis[i.first]>newTime && newTime<=maxTime){
-                    dis[i.first]=newTime;
+                if(newTime<=maxTime && dis[i.first][newTime]>newCost){
+                    dis[i.first][newTime]=newCost;
                     q.push({newCost,newTime,i.first});
                 }
             }
         }
-        return -1;
+        int mini=1e9;
+        for(int i=0;i<dis[n-1].size();i++){
+            mini=min(mini,dis[n-1][i]);
+        }
+        return mini==1e9?-1:mini;
     }
     int minCost(int maxTime, vector<vector<int>>& edges, vector<int>& passingFees) {
         int n=passingFees.size();
