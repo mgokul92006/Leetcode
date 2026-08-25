025b0765dc368251965c2a74085ba1ec3c027583
mgokul92006 <mgokul92006@gmail.com>
2026-08-25 18:07:45 +0530

    Time: 470 ms (6.62%), Space: 207.6 MB (16.1%) - LeetHub

---
+++ 3619-count-islands-with-total-value-divisible-by-k/3619-count-islands-with-total-value-divisible-by-k.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int x[4]={1,-1,0,0};
+    int y[4]={0,0,1,-1};
+    int bfs(vector<vector<int>>&grid,vector<vector<int>>&vis,int i,int j,int k){
+        queue<pair<int,int>>q;
+        q.push({i,j});
+        vis[i][j]=1;
+        long long c=grid[i][j];
+        while(!q.empty()){
+            int m=q.front().first;
+            int n=q.front().second;
+            q.pop();
+            for(int z=0;z<4;z++){
+                int x1=m+x[z];
+                int y1=n+y[z];
+                if(x1>=0 && x1<grid.size() && y1>=0 && y1<grid[0].size() && grid[x1][y1]!=0 && vis[x1][y1]==0){
+                    vis[x1][y1]=1;
+                    q.push({x1,y1});
+                    c=c+grid[x1][y1];
+                }
+            }
+        }
+        cout<<c<<" ";
+        return c%k==0;
+    }
+    int countIslands(vector<vector<int>>& grid, int k) {
+        vector<vector<int>>vis(grid.size(),vector<int>(grid[0].size(),0));
+        int maxi=0;
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                if(grid[i][j]!=0 && vis[i][j]==0){
+                    maxi=maxi+bfs(grid,vis,i,j,k);
+                }
+            }
+        }
+        return maxi;
+    }
+};
