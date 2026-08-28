599d91866bbf842de70940dd2054d135ecc92007
mgokul92006 <mgokul92006@gmail.com>
2026-08-28 15:24:07 +0530

    Time: 34 ms (84.2%), Space: 21.8 MB (93.91%) - LeetHub

--- 1654-minimum-jumps-to-reach-home/1654-minimum-jumps-to-reach-home.cpp
+++ 1654-minimum-jumps-to-reach-home/1654-minimum-jumps-to-reach-home.cpp
@@ -4,26 +4,26 @@ public:
         queue<tuple<int,int,int>>q;
         q.push({0,0,0});
         int limit=a+b+max(x,*max_element(s.begin(),s.end()));
-        vector<vector<int>>vis(limit+1,vector<int>(2,0));
         s.insert(0);
         while(!q.empty()){
             auto [back,node,step]=q.front();
             q.pop();
             if(node==x)
-            return step;
-            if(s.find(node+a)==s.end() && node+a<=limit && vis[node+a][0]==0){
-                q.push({0,node+a,step+1});
-                vis[node+a][0]=1;
-            }
-            if(back==0 && node-b>=0 && s.find(node-b)==s.end() && vis[node-b][1]==0){
-                vis[node-b][1]=1;
+                return step;
+            if( back == 0 && node-b >= 0 && node - b <= limit && s.find(node-b) == s.end()){
+                s.insert(node-b);
                 q.push({1,node-b,step+1});
             }
+            if(s.find(node+a) == s.end() && node + a >= 0 && node+a <= limit){
+                q.push({0,node+a,step+1});
+                s.insert(node+a);
+            }
+           
         }
         return -1;
     }
     int minimumJumps(vector<int>& forbidden, int a, int b, int x) {
-        set<int>s(forbidden.begin(),forbidden.end());
+        set<int> s(forbidden.begin(),forbidden.end());
         return bfs(s,a,b,x);
     }
 };
