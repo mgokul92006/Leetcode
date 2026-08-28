8eff762e4f665d7eb795130f9cdc0ba4244576dc
mgokul92006 <mgokul92006@gmail.com>
2026-08-28 14:10:40 +0530

    Time: 66 ms (61.4%), Space: 36.8 MB (68.62%) - LeetHub

---
+++ 1654-minimum-jumps-to-reach-home/1654-minimum-jumps-to-reach-home.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int bfs(set<int>&s,int a,int b,int x){
+        queue<tuple<int,int,int>>q;
+        q.push({0,0,0});
+        int limit=a+b+max(x,*max_element(s.begin(),s.end()));
+        vector<vector<int>>vis(limit+1,vector<int>(2,0));
+        s.insert(0);
+        while(!q.empty()){
+            auto [back,node,step]=q.front();
+            q.pop();
+            if(node==x)
+            return step;
+            if(s.find(node+a)==s.end() && node+a<=limit && vis[node+a][0]==0){
+                q.push({0,node+a,step+1});
+                vis[node+a][0]=1;
+            }
+            if(back==0 && node-b>=0 && s.find(node-b)==s.end() && vis[node-a][1]==0){
+                vis[node-a][1]=1;
+                q.push({1,node-b,step+1});
+            }
+        }
+        return -1;
+    }
+    int minimumJumps(vector<int>& forbidden, int a, int b, int x) {
+        set<int>s(forbidden.begin(),forbidden.end());
+        return bfs(s,a,b,x);
+    }
+};
