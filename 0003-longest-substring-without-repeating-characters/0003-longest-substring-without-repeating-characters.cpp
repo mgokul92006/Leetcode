1d2cd8001e82049f024e90986d0119eb53036e55
mgokul92006 <mgokul92006@gmail.com>
2026-09-01 13:56:47 +0530

    Time: 195 ms (19.91%), Space: 56.7 MB (19.59%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -7,6 +7,8 @@ public:
             mp[s[r]]++;
             while(mp[s[r]]>1){
                 mp[s[l]]--;
+                if(mp[s[l]]==0)
+                mp.erase(s[l]);
                 l++;
             }
             maxi=max(maxi,r-l+1);
