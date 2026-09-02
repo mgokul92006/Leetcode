ae6402cdfae9b69d6f32baa56a65931c00c82efc
mgokul92006 <mgokul92006@gmail.com>
2026-09-02 16:09:00 +0530

    Time: 0 ms (100%), Space: 8.6 MB (79.84%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int dpCalculate(string& s,int i,vector<int>&dp){
+        if(i>=s.size())
+        return 1;
+        if(s[i]=='0')
+        return 0;
+        if(dp[i]!=-1)
+        return dp[i];
+        int take=0,nottake=0;
+        take=take+dpCalculate(s,i+1,dp);
+        if(i+1<s.size() && (s[i]=='1'|| s[i]=='2' && s[i+1]<='6'))
+        nottake=nottake+dpCalculate(s,i+2,dp);
+        return dp[i]=take+nottake;
+    }
+    int numDecodings(string s) {
+        vector<int>dp(s.size(),-1);
+        return dpCalculate(s,0,dp);
+    }
+};
