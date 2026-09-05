bc50974ab22113b0d0f0c66ca8ec0c65c09785dd
mgokul92006 <mgokul92006@gmail.com>
2026-09-05 14:02:00 +0530

    Time: 31 ms (26.31%), Space: 79.2 MB (11.45%) - LeetHub

--- 1109-corporate-flight-bookings/1109-corporate-flight-bookings.cpp
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
-        vector<int>k(n+1,0);
-        for(auto a:bookings){
-            int f=a[0];
-            int l=a[1];
-            int v=a[2];
-            k[f-1]+=v;
-            if(l<n){
-                k[l]-=v;
-            }
+        vector<int>arr(n+1,0);
+        for(auto i:bookings){
+            arr[i[0]-1]=arr[i[0]-1]+i[2];
+            if(i[1]<n)
+                arr[i[1]]=arr[i[1]]-i[2];
         }
         vector<int>ans(n,0);
-        ans[0]=k[0];
+        ans[0]=arr[0];
         for(int i=1;i<n;i++){
-            ans[i]=ans[i-1]+k[i];
+            ans[i]=ans[i-1]+arr[i];
         }
         return ans;
     }
